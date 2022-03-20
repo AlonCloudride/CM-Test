420a90ab550bfc84d853cea4c46b44ec7737dc93
AlonCloudride <alon@cloudride.co.il>
2022-03-20 20:02:47 +0200

    Update after_install.sh
    if section commented

--- after_install.sh
+++ after_install.sh
@@ -9,11 +9,11 @@ sudo chown -R nginx:nginx $DEPLOYMENT_SRC
 find $DEPLOYMENT_SRC -type d -exec chmod 770 {} \;
 find $DEPLOYMENT_SRC -type f -exec chmod 660 {} \;
 
-if [ ! -f $DEPLOYMENT_SRC/composer.json ] || [ -f $DEPLOYMENT_SRC/composer.phar ];
-then
-        echo "Cant find vandor"
-        exit 1
-fi
+# if [ ! -f $DEPLOYMENT_SRC/composer.json ] || [ -f $DEPLOYMENT_SRC/composer.phar ];
+# then
+#         echo "Cant find vandor"
+#         exit 1
+# fi
 
 echo "Move folder into place"
 mv $DEPLOYMENT_DST $DEPLOYMENT_TMP && mv $DEPLOYMENT_SRC $DEPLOYMENT_DST && rm -rf $DEPLOYMENT_TMP
