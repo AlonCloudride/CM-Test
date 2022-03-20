4ecfea577bc82a11b607b8783de98bd06d6e2bfb
AlonCloudride <alon@cloudride.co.il>
2022-03-20 16:30:53 +0200

    update

--- validate_service.sh
+++ validate_service.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
+printf '%s\n' "Check web status"
+systemctl status nginx
 
-if [[ "$HOSTNAME" =~ -web$ ]]; then
-	printf '%s\n' "Check web status"
-	systemctl status nginx
-else
-	printf '%s\n' "Check worker status"
-	systemctl status ebankp-worker\@*
-fi%
+systemctl status php-fpm
+supervisorctl status
