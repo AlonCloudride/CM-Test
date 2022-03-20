3fd15f26e00bb2c246a057eb78c3f37482d12736
AlonCloudride <alon@cloudride.co.il>
2022-03-20 19:58:19 +0200

    fixes
    before_install: add composer installation
    after_install: add composer check
    add install_composer

--- after_install.sh
+++ after_install.sh
@@ -4,11 +4,16 @@ DEPLOYMENT_SRC=/tmp/deploy-$DEPLOYMENT_GROUP_ID-$DEPLOYMENT_ID
 DEPLOYMENT_DST=/usr/share/nginx/html/app/payment-gateway
 DEPLOYMENT_TMP=`mktemp -d`
 
-echo "Fix permissions"
+echo "Fixing permissions"
 sudo chown -R nginx:nginx $DEPLOYMENT_SRC
 find $DEPLOYMENT_SRC -type d -exec chmod 770 {} \;
 find $DEPLOYMENT_SRC -type f -exec chmod 660 {} \;
 
+if [ ! -f $DEPLOYMENT_SRC/composer.json ] || [ -f $DEPLOYMENT_SRC/composer.phar ];
+then
+        echo "Cant find vandor"
+        exit 1
+fi
 
 echo "Move folder into place"
 mv $DEPLOYMENT_DST $DEPLOYMENT_TMP && mv $DEPLOYMENT_SRC $DEPLOYMENT_DST && rm -rf $DEPLOYMENT_TMP
--- before_install.sh
+++ before_install.sh
@@ -6,4 +6,15 @@ DEPLOYMENT_DST=/tmp/deploy-$DEPLOYMENT_GROUP_ID-$DEPLOYMENT_ID
 mkdir $DEPLOYMENT_DST
 
 echo "Copy to tmp deployment folder"
-rsync -r $DEPLOYMENT_SRC/ $DEPLOYMENT_DST
+rsync -r --exclude-from=$DEPLOYMENT_SRC/deploy/.exclude $DEPLOYMENT_SRC/ $DEPLOYMENT_DST
+
+cd $DEPLOYMENT_DST
+
+echo "Install composer"
+/bin/sh $DEPLOYMENT_SRC/install_composer.sh
+
+echo "Run composer install"
+./composer.phar i --no-interaction --prefer-dist --optimize-autoloader
+
+echo "Remove composer.phar"
+rm -f composer.phar
