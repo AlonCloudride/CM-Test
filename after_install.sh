9f7182d7cf12434a94b8973dc6348e70488dd89a
AlonCloudride <alon@cloudride.co.il>
2022-03-20 19:47:36 +0200

    Update after_install.sh

--- after_install.sh
+++ after_install.sh
@@ -5,15 +5,10 @@ DEPLOYMENT_DST=/usr/share/nginx/html/app/payment-gateway
 DEPLOYMENT_TMP=`mktemp -d`
 
 echo "Fix permissions"
-chown -R nginx:nginx $DEPLOYMENT_SRC
+sudo chown -R nginx:nginx $DEPLOYMENT_SRC
 find $DEPLOYMENT_SRC -type d -exec chmod 770 {} \;
 find $DEPLOYMENT_SRC -type f -exec chmod 660 {} \;
 
-if [ ! -d $DEPLOYMENT_SRC/vendor ];
-then
-        echo "Cant find vandor"
-        exit 1
-fi
 
 echo "Move folder into place"
 mv $DEPLOYMENT_DST $DEPLOYMENT_TMP && mv $DEPLOYMENT_SRC $DEPLOYMENT_DST && rm -rf $DEPLOYMENT_TMP
