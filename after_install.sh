d8c45ce0c5c808a306954dcff236d37cb333822a
AlonCloudride <alon@cloudride.co.il>
2022-03-20 14:00:43 +0200

    fixed workflow.yml and added run command to after_install.sh, and renamed helloworld.html -> index.html

--- after_install.sh
+++ after_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 DEPLOYMENT_SRC=/tmp/deploy-$DEPLOYMENT_GROUP_ID-$DEPLOYMENT_ID
-DEPLOYMENT_DST=/var/www/html
+DEPLOYMENT_DST=/usr/share/nginx/html/app/payment-gateway
 DEPLOYMENT_TMP=`mktemp -d`
 
 echo "Fix permissions"
@@ -17,3 +17,9 @@ fi
 
 echo "Move folder into place"
 mv $DEPLOYMENT_DST $DEPLOYMENT_TMP && mv $DEPLOYMENT_SRC $DEPLOYMENT_DST && rm -rf $DEPLOYMENT_TMP
+
+php artisan optimize:clear
+php artisan optimize
+composer dump-autoload -o
+sudo systemctl restart nginx php-fpm
+sudo supervisorctl restart all
