d02f18e8e70db6876679c2823d720aafc0163538
AlonCloudride <alon@cloudride.co.il>
2022-03-20 21:06:44 +0200

    Update validate_service.sh

--- validate_service.sh
+++ validate_service.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-printf '%s\n' "Check web status"
-systemctl status nginx
+echo "heelo"
+# printf '%s\n' "Check web status"
+# systemctl status nginx
 
-systemctl status php-fpm
-supervisorctl status
+# systemctl status php-fpm
+# supervisorctl status
