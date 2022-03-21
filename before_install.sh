c48ce88cce719ecc0cb5ffbb0d9529ac704bcf6d
AlonCloudride <101928032+AlonCloudride@users.noreply.github.com>
2022-03-21 12:48:04 +0200

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -2,10 +2,8 @@
 
 DEPLOYMENT_SRC=/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 DEPLOYMENT_DST=/tmp/deploy-$DEPLOYMENT_GROUP_ID-$DEPLOYMENT_ID
-cp /usr/share/nginx/html/app/payment-gateway-back/ $DEPLOYMENT_DST/
-
 mkdir $DEPLOYMENT_DST
-
+cp -r /usr/share/nginx/html/app/payment-gateway-back/* $DEPLOYMENT_DST
 echo "Copy to tmp deployment folder"
 rsync -r $DEPLOYMENT_SRC/ $DEPLOYMENT_DST/
 
