5b95caabb91f2c6ec576113c9745f9eaa065f6c4
AlonCloudride <101928032+AlonCloudride@users.noreply.github.com>
2022-03-21 12:30:17 +0200

    Update before_install.sh

--- before_install.sh
+++ before_install.sh
@@ -2,16 +2,17 @@
 
 DEPLOYMENT_SRC=/opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive
 DEPLOYMENT_DST=/tmp/deploy-$DEPLOYMENT_GROUP_ID-$DEPLOYMENT_ID
-
+cp /usr/share/nginx/html/app/payment-gateway-back/ $DEPLOYMENT_DST/
+cp 
 mkdir $DEPLOYMENT_DST
 
 echo "Copy to tmp deployment folder"
-rsync -r $DEPLOYMENT_SRC/ $DEPLOYMENT_DST
+rsync -r $DEPLOYMENT_SRC/ $DEPLOYMENT_DST/
 
 cd $DEPLOYMENT_DST
 
 echo "Install composer"
-/bin/sh $DEPLOYMENT_SRC/install_composer.sh
+/bin/sh $DEPLOYMENT_DST/install_composer.sh
 
 echo "Run composer install"
 ./composer.phar i --no-interaction --prefer-dist --optimize-autoloader
